4049da5510642e6b75116102c5748d2937c5697c
Daniel Muñoz <info@gmail.com>
2020-01-20 22:13:24 +0100

    project finished

--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -50,20 +50,27 @@
 
         <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
           <button
+            @click="toggleConverter"
             class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
           >
-            Cambiar
+            {{ fromUsd ? `USD a ${asset.symbol}` : `${asset.symbol} a USD` }}
           </button>
 
           <div class="flex flex-row my-5">
             <label class="w-full" for="convertValue">
               <input
+                v-model="convertValue"
                 id="convertValue"
                 type="number"
                 class="text-center bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+                :placeholder="` Valor en ${fromUsd ? 'USD' : asset.symbol}`"
               />
             </label>
           </div>
+
+          <span class="text-xl">
+            {{ converResult }} {{ fromUsd ? asset.symbol : 'USD' }}</span
+          >
         </div>
       </div>
       <line-chart
@@ -124,11 +131,25 @@ export default {
       isLoading: false,
       asset: {},
       history: [],
-      markets: []
+      markets: [],
+      fromUsd: true,
+      convertValue: null
     }
   },
 
   computed: {
+    converResult() {
+      if (!this.convertValue) {
+        return 0
+      }
+
+      const result = this.fromUsd
+        ? this.convertValue / this.asset.priceUsd
+        : this.convertValue * this.asset.priceUsd
+
+      return result.toFixed(4)
+    },
+
     chartData() {
       const data = []
       this.history.map(h => {
@@ -159,7 +180,16 @@ export default {
     this.getCoin()
   },
 
+  watch: {
+    $route() {
+      this.getCoin()
+    }
+  },
+
   methods: {
+    toggleConverter() {
+      this.fromUsd = !this.fromUsd
+    },
     getCoin() {
       let id = this.$route.params.id
       this.isLoading = true
